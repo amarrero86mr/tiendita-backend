963d3f2a8dabc1ed1882c69ac5f44c07094973ad
amarrero86mr <amarrero86mr@gmail.com>
2025-10-03 13:21:27 -0300

    implementado metodos de post, put, y delete en swagger

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -57,18 +57,119 @@ PRODUCT_ROUTER.get("/all/:page?/:items_per_page?", async (req, res) => {
   );
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Return a products by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Products ID
+ *     responses:
+ *       200:
+ *         description: Products found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Products'
+ *       404:
+ *         description: Products not found
+ */
 PRODUCT_ROUTER.get("/:id", async (req, res) => {
   endpointResolver(req, res, await getProductById(req.params.id));
 });
 
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new products
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Products'
+ *             example: { "id_product": 20, "product_name": "Insecticida Raid", "product_description": "Aerosol contra mosquitos y cucarachas 360ml", "stock": 300, "price": 2800 }
+ *     responses:
+ *       201:
+ *         description: Client created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Clients'
+ *       400:
+ *         description: Bad Request
+ */
 PRODUCT_ROUTER.post("/", async (req, res) => {
   endpointResolver(req, res, await postProduct(req.body));
 });
 
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Deleting a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Products ID
+ *     responses:
+ *       200:
+ *         description: Products deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Products'
+ *       404:
+ *         description: Products not found
+ */
 PRODUCT_ROUTER.delete("/:id", async (req, res) => {
   endpointResolver(req, res, await deleteProduct(req.params.id));
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Products ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Products'
+ *             example: { "id_product": 20, "product_name": "Insecticida Raid", "product_description": "Aerosol contra mosquitos y cucarachas 360ml", "stock": 300, "price": 2800 }
+ *     responses:
+ *       201:
+ *         description: Products created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               example: { "id_product": 20, "product_name": "Insecticida Raid", "product_description": "Aerosol contra mosquitos y cucarachas 360ml", "stock": 300, "price": 2800 }
+ *               $ref: '#/components/schemas/Products'
+ *       400:
+ *         description: Bad Request
+ */
 PRODUCT_ROUTER.put("/:id", async (req, res) => {
   endpointResolver(req, res, await editProduct(req.params.id, req.body));
 });
